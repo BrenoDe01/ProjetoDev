d8439b73d1f8d56e771ba112bbf6d40438f9b73d
BrenoDe01 <brenoln13@gmail.com>
2023-06-12 19:58:19 -0300

    first commit

---
+++ src/test/java/ProjetoDev/ProjetoDev1/ProjetoDev1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjetoDev.ProjetoDev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDev1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
